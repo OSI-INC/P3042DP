55b7aac225e5f3b73cbd6f7917ff4dba5d18e235
Kevan Hashemi <hashemi@opensourceinstruments.com>
2022-08-24 22:39:34 -0400

    Create Main.vhdl and project.

---
+++ Main.vhd
@@ -0,0 +1,39 @@
+-- <pre> Telemetry Control Box (TCB) Display Panel (A3042DP) Firmware, Toplevel Unit
+
+-- V1.1 [24-AUG-22] Starting point for development. Defines inputs and outputs.
+
+library ieee;  
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity main is 
+	port (
+		CK -- Clock
+		: in std_logic; 
+		TP1, -- Test Point One (TMS)
+		TP2, -- Test Point Two (TDI)
+		TP3, -- Test Point Three (TDO)
+		TP4  -- Test Point Four (TCK)
+		: out std_logic
+	);
+end;
+
+architecture behavior of main is
+
+begin
+
+
+
+-- Test Point One appears on P1-6.
+	TP1 <= CK;
+	
+-- Test Point Two appears on P1-3.
+	TP2 <= CK;
+	
+-- Test Point Three appears on P1-2.
+	TP3 <= CK;
+
+-- Test Point Four appears on P1-8.
+	TP4 <= CK;
+	
+end behavior;
